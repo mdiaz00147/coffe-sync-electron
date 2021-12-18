de634a7df35d66ce3f7ac6e4f3c6874beb402104
Mario <mario@blackboxcode.io>
2021-12-18 15:32:03 -0500

    local

---
+++ server/index.js
@@ -0,0 +1,23 @@
+let express = require("express");
+var bodyParser = require("body-parser");
+const cors = require('cors');
+
+const { routes } = require("./src/routes");
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+// app routes
+routes(app);
+
+let server = app.listen(3020, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log("App Listening at http://%s:%s", host, port);
+});
---
+++ server/scripts/deploy-prod.sh
@@ -0,0 +1,2 @@
+# upload to server
+rsync -azP ./../home-iptv root@80.82.76.235:/home/
