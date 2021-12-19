3b5ac1c558c3265f351b2c1aaec8e00d4a821ce0
Mario <mario@blackboxcode.io>
2021-12-19 13:30:29 -0500

    show file size and navigation working

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,10 @@
 // controllers
 const downloadsFilesController = require("../controllers/api/v1/downloads/filesController");
-const filesController = require("../controllers/api/v1/filesController");
-const foldersController = require("../controllers/api/v1/foldersController");
+const listFilesController = require("../controllers/api/v1/list/filesController");
 
 exports.routes = (app) => {
   // V1 routes
-  app.route("/api/v1/folders/:folderName").get(foldersController.show);
-
-  app.route("/api/v1/files").get(filesController.index);
+  app.route("/api/v1/list/files").post(listFilesController.create);
 
   app.route("/api/v1/downloads/files").post(downloadsFilesController.create);
 };
--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,11 @@
     <v-app-bar app color="light-green darken-4" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           src="./assets/black_logo.png"
           transition="scale-transition"
           width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
 
